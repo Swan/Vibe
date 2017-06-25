550c19f42b01871b2d60998d5850fef8d39448a7
Swan <contact@swan.moe>
2017-06-25 14:24:28 -0400

    Change: More formatting fixes

--- websocket/newscore.js
+++ websocket/newscore.js
@@ -12,16 +12,14 @@ module.exports.newScore = async (client, newScoreData) => {
         if (typeof beatmapScores.scores[i].id != 'undefined' && beatmapScores.scores[i].id == newScoreData.id) {
             client.channels.get(config.trackingChannel)
                 .sendEmbed(new Discord.RichEmbed()
-                    .setTitle(`New ${addCommas(Math.trunc(newScoreData.pp))}pp score submitted by: ${userInfo.username}`)
+                    .setTitle(`New ${addCommas(Math.trunc(newScoreData.pp))}pp (#${i + 1}) score submitted by: ${userInfo.username}`)
                     .setColor(0x73C383)
                     .setThumbnail(`https://a.ripple.moe/${newScoreData.user_id}`)
-                    .addField('Details', `User: **${userInfo.username}** has submitted a **${addCommas(Math.trunc(newScoreData.pp))}pp** score!`, false)
                     .addField('Beatmap', `[${beatmapInfo.beatmap.song_name}](https://eggplants.org/b/${beatmapInfo.beatmap.beatmap_id})`, false)
-                    .addField('Profile', `[Profile](https://ripple.moe/u/${newScoreData.user_id})`, true)
+                    .addField('Profile', `[Profile](https://ripple.moe/u/${userInfo.username})`, true)                   
                     .addField('Replay', `[Replay](https://ripple.moe/web/replays/${newScoreData.id})`, true)
-                    .addField('PP', `${addCommas(Math.trunc(newScoreData.pp))}`, true)
-                    .addField('Rank/Acc/Score/Combo', `${newScoreData.rank}/${newScoreData.accuracy}%/${addCommas(newScoreData.score)}/${addCommas(newScoreData.max_combo)}`, true)
-                    .addField('Acc Spread', `${addCommas(newScoreData.count_geki)}/${addCommas(newScoreData.count_300)}/${addCommas(newScoreData.count_katu)}/${addCommas(newScoreData.count_100)}/${addCommas(newScoreData.count_50)}/${addCommas(newScoreData.count_miss)}`, true))
+                    .addField('Score', `${addCommas(newScoreData.score)}/${(Math.floor(newScoreData.accuracy * 100) / 100)}% ${newScoreData.rank}`, true)
+                    .addField('Accuracy Spread', `${addCommas(newScoreData.max_combo)}x/${addCommas(newScoreData.count_geki)}/${addCommas(newScoreData.count_300)}/${addCommas(newScoreData.count_katu)}/${addCommas(newScoreData.count_100)}/${addCommas(newScoreData.count_50)}/${addCommas(newScoreData.count_miss)}`, true))
         }
     }
 
